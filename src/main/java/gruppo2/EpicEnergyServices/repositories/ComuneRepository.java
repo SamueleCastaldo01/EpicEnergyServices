192f4a797712ec806c3ba826c7439ea569c7dd92
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-11 13:18:45 +0100

    commit comune

--- src/main/java/gruppo2/EpicEnergyServices/repositories/ComuneRepository.java
+++ src/main/java/gruppo2/EpicEnergyServices/repositories/ComuneRepository.java
@@ -1,8 +1,9 @@
 package gruppo2.EpicEnergyServices.repositories;
 
+import gruppo2.EpicEnergyServices.entities.Comune;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ComuneRepository extends JpaRepository<ComuneRepository, Long> {
+public interface ComuneRepository extends JpaRepository<Comune, Long> {
 }

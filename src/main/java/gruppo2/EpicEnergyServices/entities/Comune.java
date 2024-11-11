bf0e611bc78605437111015be99917bebe3257ae
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-11 13:11:44 +0100

    commit comune

--- src/main/java/gruppo2/EpicEnergyServices/entities/Comune.java
+++ src/main/java/gruppo2/EpicEnergyServices/entities/Comune.java
@@ -15,4 +15,37 @@ public class Comune {
 
     public Comune() {}
 
+    public Comune(Provincia provincia, String comune) {
+        this.provincia = provincia;
+        this.comune = comune;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Provincia getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(Provincia provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    @Override
+    public String toString() {
+        return "Comune{" +
+                "id=" + id +
+                ", provincia=" + provincia +
+                ", comune='" + comune + '\'' +
+                '}';
+    }
 }

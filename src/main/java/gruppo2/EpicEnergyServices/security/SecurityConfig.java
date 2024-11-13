6c41cecdd909c38e808ebde1b1c63b62562b3d88
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-13 10:18:04 +0100

    Commit permessi

--- src/main/java/gruppo2/EpicEnergyServices/security/SecurityConfig.java
+++ src/main/java/gruppo2/EpicEnergyServices/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package gruppo2.EpicEnergyServices.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
